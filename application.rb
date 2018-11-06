877cb4450c51ab79783f4b2fe049dfffbb9ac8aa
Justin Kenney <justinkenney13@gmail.com>
2018-11-06 16:28:18 +0000

    Done.

--- application.rb
+++ application.rb
@@ -2,9 +2,9 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    # resp.write "Hello world!"
+    resp.write "Hello, my name is Justin Awesome Kenney!"
     resp.finish
   end
 
 end
-
